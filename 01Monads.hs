04abfc01827ab8573728bd948ede52fbf75c68ff
unknown <Lesja@Lesja-PC.(none)>
2012-05-11 11:59:04 +0200

    first commit

---
+++ 01Monads.hs
@@ -0,0 +1,226 @@
+-- | Functional Programming, course at LMU, summer term 2012
+--   Andreas Abel and Steffen Jost
+--
+-- Exercise sheet 1, 2012-04-25
+--
+-- Instructions:
+--
+-- Replace all occurrences of 'undefined' and 'Undefined' by sensible code.
+-- Do not change the type of functions you are asked to implement!
+-- Quickcheck test cases may help you finding bugs.
+--
+-- Spell out proofs where asked for.
+--
+-- Submit your solutions via UniWorX.
+
+----------------------------------------------------------------------
+-- Header
+--
+-- You can add imports and LANGUAGE pragmas here.
+----------------------------------------------------------------------
+
+{-# LANGUAGE TemplateHaskell #-}
+
+module Monads where
+
+import Control.Monad.Error
+
+import qualified Data.Set as Set
+
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+data Undefined
+
+----------------------------------------------------------------------
+-- Exercise 1
+----------------------------------------------------------------------
+
+-- a) Define functoriality from a monad
+
+infixl 5 <$>
+
+-- | Map function for monads.
+(<$>) :: Monad m => (a -> b) -> m a -> m b
+f <$> m = do x <- m
+             return $ f x
+
+-- b) For your implementation, prove the functor laws from the monad laws
+
+{-
+Identity:
+  id <$> m = m
+
+let m1 = id <$> m = do x <- m
+                       return $ id x
+
+Nach "Links-Eins" ist m1 = id m = m
+
+Composition:
+  f <$> (g <$> m) = f . g <$> m
+
+1) let m1 = g <$> m = do x <- m
+                         return $ g x
+
+Nach "Links-Eins" ist m1 = g m
+
+2) let m2 = f <$> m1 = do x <- m1               
+                          return f x            
+
+Nach "Links-Eins" ist m2 = f m1 = f . g m
+
+3) let m3 = f . g <$> m = do x <- m
+                             return f . g x  
+
+Nach "Links-Eins" ist m3 = f . g m   
+
+Also, m2 = m3            
+
+-}
+
+-- Tests for Exercise 1 ----------------------------------------------
+
+prop_mmap_identity m =
+  id <$> m == (m :: [Int])
+
+-- TEST prop_mmap_identity [1,3,5]
+
+prop_mmap_composition f g m =
+  f <$> (g <$> m) == f . g <$> (m :: [Int])
+
+-- TEST prop_mmap_composition (\x -> 2*x) (\x -> x*x) [1,3,5]
+
+----------------------------------------------------------------------
+-- Exercise 2
+----------------------------------------------------------------------
+
+-- Define the Monad and MonadPlus instances for the following
+-- Option type constructor.
+
+data Option a = None | Some a
+  deriving (Show, Eq, Ord)
+
+bindO :: Option a -> (a -> Option b) -> Option b
+bindO None k = None
+bindO (Some a) k = k a
+
+instance Monad Option where
+  return a = Some a
+  (>>=) = bindO
+
+instance MonadPlus Option where
+  mzero = None
+  mplus mzero    (Some a) = Some a 
+  mplus (Some a) mzero    = Some a
+  mplus a        b        = a
+
+-- Tests for Exercise 2 ----------------------------------------------
+
+instance Arbitrary a => Arbitrary (Option a) where
+  arbitrary = frequency [(1, return None), (4, fmap Some arbitrary)]
+  shrink _  = []
+
+instance Show (a -> b) where
+  show f = "<function>"
+
+-- Monad laws
+prop_monad_left a k =
+  (return a >>= k)         == (k a :: Option Int)
+prop_monad_right m =
+  (m >>= return)           == (m   :: Option Int)
+prop_monad_assoc m k l =
+  (m >>= \ x -> k x >>= l) == (m >>= k >>= l :: Option Int)
+
+-- MonadZero laws
+prop_mzero_left  m = mzero `mplus` m == (m :: Option Int)
+prop_mzero_right m = m `mplus` mzero == (m :: Option Int)
+prop_mzero_assoc m1 m2 m3 =
+  m1 `mplus` (m2 `mplus` m3) == (m1 `mplus` m2) `mplus` (m3 :: Option Int)
+
+-- MonadZero bind laws
+prop_zero_bind_l k   = (mzero >>= k) == (mzero :: Option Int)
+prop_zero_bind_r m   = (m >> mzero)  == (mzero :: Option Int)
+
+-- Left distribution law does not hold for Option
+-- prop_plus_dist_l m n k =
+--   ((m `mplus` n) >>= k) == ((m >>= k) `mplus` (n >>= k) :: Option Int)
+prop_plus_dist_r m k l =
+  (m >>= \ x -> k x `mplus` l x) == ((m >>= k) `mplus` (m >>= l) :: Option Int)
+
+----------------------------------------------------------------------
+-- Exercise 3
+----------------------------------------------------------------------
+
+-- | Class of service monads that generate unique Integer numbers.
+class Monad m => MonadUnique m where
+  unique :: m Integer
+
+newtype Unique a = Unique Undefined
+
+instance Monad Unique where
+  return = undefined
+  (>>=)  = undefined
+
+instance MonadUnique Unique where
+  unique = undefined
+
+runUnique :: Unique a -> a
+runUnique = undefined
+
+-- Tests for Exercise 3 ----------------------------------------------
+
+-- | @uniques n@ generates a list of @n@ different @Integer@s.
+uniques :: Int -> [Integer]
+uniques n = runUnique $ forM [1..n] $ const unique
+
+-- | @allDifferent l@ checks that list @l@ does not contain duplicates.
+--   O(n log n), but not implemented efficiently.
+allDifferent :: (Eq a, Ord a) => [a] -> Bool
+allDifferent l = l == Set.toList (Set.fromList l)
+
+-- | Test that numbers generated by @unique@ are in fact unique.
+prop_unique = forAll (choose (1,100)) $ \ n ->
+  allDifferent (uniques n)
+
+----------------------------------------------------------------------
+-- Exercise 4  Output monad
+----------------------------------------------------------------------
+
+-- Implement a monad that lets you pay for computation
+
+-- | Abstract monad for accumulating costs.
+class Monad m => MonadCost m where
+  pay :: Integer -> m ()
+
+newtype Cost a = Cost Undefined
+
+instance Monad Cost where
+  return a = undefined
+  m >>= k  = undefined
+
+instance MonadCost Cost where
+  pay n = undefined
+
+runCost :: Cost a -> (a, Integer)
+runCost = undefined
+
+-- | @revAppSum xs acc@ reverses @xs@ onto @acc@ and 'pay's amount @x@
+--   for each element @x@ of the list @xs@ that is touched.
+revAppSum :: MonadCost m => [Integer] -> [Integer] -> m [Integer]
+revAppSum = undefined
+
+-- | @reverseSum xs@ returns @(reverse xs, sum xs)@ which is computed
+--   by a call to 'revAppSum'.
+reverseSum :: [Integer] -> ([Integer], Integer)
+reverseSum xs = runCost $ revAppSum xs []
+
+-- Tests for Exercise 4 ----------------------------------------------
+
+prop_reverseSum xs = reverseSum xs == (reverse xs, sum xs)
+
+----------------------------------------------------------------------
+-- Haskell footer
+----------------------------------------------------------------------
+
+-- | Runs all tests starting with "prop_" in this file.
+runTests = $quickCheckAll
---
+++ 02Sheet.hs
@@ -0,0 +1,142 @@
+-- | Functional Programming, course at LMU, summer term 2012
+--   Andreas Abel and Steffen Jost
+--
+-- Exercise sheet 2, 2012-05-03
+--
+-- Instructions:
+--
+-- Replace all occurrences of 'undefined' and 'Undefined' by sensible code.
+-- Do not change the type of functions you are asked to implement!
+--
+-- Submit your solutions via UniWorX.
+
+----------------------------------------------------------------------
+-- Header
+--
+-- You can add imports and LANGUAGE pragmas here.
+----------------------------------------------------------------------
+
+module Main where
+
+import Control.Applicative ((<$>))
+import Control.Monad
+
+import qualified Data.List as List
+
+import Debug.Trace
+
+import System.Directory
+import System.Environment
+import System.FilePath
+
+----------------------------------------------------------------------
+-- Exercise 1  Sudoku-Board as list of lists
+----------------------------------------------------------------------
+
+-- | A completely filled Sudoku board is represented as a list of @n*n@ rows
+--   which are each a list of @n*n@ numbers between 1 and @n*n@ (inclusively).
+type Sudoku = [Row]
+type Row    = [Int]
+
+-- | A (completely filled) Sudoku board is valid if all its @rows@, @columns@
+--   ans @n×n@-@blocks@ are permutations of @1..n*n@.
+checkRow :: Int -> Row -> Bool
+checkRow n x = List.sort x == [1 .. n*n]
+
+checkRows :: Int -> Sudoku -> Bool
+checkRows n [] = True
+checkRows n (x:xs) = (checkRow n x) && checkRows n xs 
+
+boxes :: Int -> Sudoku -> Sudoku
+boxes n s = boxes_acc n s []
+
+boxes_acc :: Int -> Sudoku -> [Sudoku] -> Sudoku
+boxes_acc n s acc | length (head s) == 0 = concatBox n acc []
+                  | otherwise            = boxes_acc n s1 (x:acc)
+    where (x, s1) = (map (take n) s, map (drop n) s) 
+
+concatBox :: Int -> [Sudoku] -> [Sudoku] -> Sudoku 
+concatBox n []   acc = concat acc
+concatBox n (x:xs) acc = concatBox n xs ((merge n x []):acc) 
+    where merge n [] acc = acc
+          merge n xs acc = merge n (drop n xs) ((concat (take n xs)):acc)
+
+validSudoku :: Int -> Sudoku -> Bool
+validSudoku n s = (checkRows n s) && (checkRows n (List.transpose s)) && (checkRows n (boxes n s))
+
+-- TEST validSudoku 2 [[1,2,3,4],[4,3,2,1],[2,1,4,3],[3,4,1,2]]
+-- TEST validSudoku 2 [[2,1,3,4],[4,3,2,1],[2,1,4,3],[3,4,1,2]]
+
+----------------------------------------------------------------------
+-- Exercise 2 Printing the directory tree
+----------------------------------------------------------------------
+
+-- | Print the directory tree recursively, starting with directory @root@.
+--   Hidden files are directories are skipped.
+--
+--   For instance, a directory structure
+--
+--     @
+--     root
+--       aDir
+--         aFile1
+--       bDir
+--         cDir
+--           cFile1
+--       dFile
+--
+--     @
+--
+--  is printed as
+--
+--    @
+--       aDir
+--       bDir
+--         cDir
+--    @
+--
+--  with sensible indentation to express child and sibling relations.
+mainDir :: FilePath -> IO ()
+mainDir root = undefined
+
+main :: IO ()
+main = do
+  args <- getArgs
+  mainDir $ if null args then "." else head args
+
+----------------------------------------------------------------------
+-- Exercise 3  Lazy monadic Boolean operators
+----------------------------------------------------------------------
+
+-- Conjunction and Disjunction are lazy
+
+verbose :: Bool -> Bool
+verbose b = trace ("encountered " ++ show b) b
+
+tA = [True, False, True, False]
+tO = [False, True, False, True]
+
+testA = and $ map verbose tA
+testO = or  $ map verbose tO
+
+-- Naive adaption to a monad makes them strict
+
+verboseM :: Bool -> IO Bool
+verboseM b = putStrLn ("encountered " ++ show b) >> return b
+
+testMA = and <$> mapM verboseM tA
+testMO = or  <$> mapM verboseM tO
+
+-- Implement lazy monadic conjunction and disjunction!
+
+andM :: Monad m => [m Bool] -> m Bool
+andM = undefined
+
+orM  :: Monad m => [m Bool] -> m Bool
+orM  = undefined
+
+-- The following test cases should only print those Booleans
+-- that are necessary to determine the result of the operation!
+
+testLA = andM $ map verboseM tA
+testLO = orM  $ map verboseM tO
